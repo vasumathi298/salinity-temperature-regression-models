d48d31a9c743039efe435e07c923bad645b822b7
Vasumathi Narayanan <72432533+vasumathi298@users.noreply.github.com>
2021-07-09 19:17:41 +0530

    Add files via upload

---
+++ regression-models/9.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from sklearn import metrics
+
+
+balance_data = pd.read_csv('bottle.csv')
+balance_data.fillna(0, inplace=True)
+X = balance_data.values[:, 0:3]
+Y = balance_data.values[:, 2]
+print(len(X),len(Y))
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.003, random_state=0)
+
+regressor = ElasticNet()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+plt.plot(y_test,label='actual',ls=('dashed'),color='black')
+plt.plot(y_pred,label='predicted',color='black')
+plt.legend()
+plt.show()
+
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
